c6d9f42a4a0819a61f9653c66d4e042ec7e74d85
Csaba Lakatos <lacsaba@gmail.com>
2018-03-16 20:50:08 +0100

    added solution for the PermMissingElem exercise.

--- 03.TimeComplexity.js
+++ 03.TimeComplexity.js
@@ -23,3 +23,13 @@ function tapeEquilibrium(A) {
 function frogJmp(X, Y, D) {
   return X === Y ? 0 : Math.ceil((Y - X) / D);
 }
+
+function permMissingElem(A) {
+  if (!A.length) return 1;
+  if (A.length === 1) return A[0] === 1 ? 2 : 1;
+
+  let sumShouldBe = (A.length + 1) * (A.length + 2) / 2;
+  let sum = A.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+  return sumShouldBe - sum;
+}
